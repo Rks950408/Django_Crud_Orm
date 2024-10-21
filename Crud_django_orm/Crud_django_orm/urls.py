47505f67505659c0770cf04e5075047d174ede0f
Rahul Kumar <rks950408@gmail.com>
2024-10-22 00:37:40 +0530

    fit commit

--- Crud_django_orm/Crud_django_orm/urls.py
+++ Crud_django_orm/Crud_django_orm/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crud_app/',include('crud_app.urls')),  
+    path('crud_app/',include('crud_app.urls')), 
+    path('', include('crud_app.urls')),  
 ]
--- Crud_django_orm/crud_app/urls.py
+++ Crud_django_orm/crud_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('create_emp/', views.create, name='create'), 
-    path('retrieve_emp/', views.reterive, name='retrieve'),  
+    path('retrieve_emp/', views.reterive, name='retrieve'),
+    path('',views.reterive, name='retrieve'),  
     path('delete_emp/<int:emp_id>/', views.delete_emp, name='delete_emp'), 
     path('update/<int:emp_id>/', views.update_emp, name='update'), 
 

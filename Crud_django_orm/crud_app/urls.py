8829d50698546dac2dcaf18b7049e0ab1bbf9d3a
Rahul Kumar <rks950408@gmail.com>
2024-10-22 00:06:51 +0530

    all update now all changes

--- Crud_django_orm/crud_app/urls.py
+++ Crud_django_orm/crud_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create_emp/', views.create, name='create'), 
     path('retrieve_emp/', views.reterive, name='retrieve'),  
     path('delete_emp/<int:emp_id>/', views.delete_emp, name='delete_emp'), 
+    path('update/<int:emp_id>/', views.update_emp, name='update'), 
+
 ]
--- Crud_django_orm/crud_app/views.py
+++ Crud_django_orm/crud_app/views.py
@@ -32,3 +32,26 @@ def delete_emp(request, emp_id):
     except tbl_employee.DoesNotExist:
         # Handle the case where the employee doesn't exist
         return render(request, 'error.html', {'message': 'Employee not found'})
+
+
+def update_emp(request, emp_id):
+    if request.method == 'POST':
+        print(request.POST, '----------------------\n')
+        e_name = request.POST.get('emp_name')
+        e_email = request.POST.get('emp_email')
+        e_mobile = request.POST.get('emp_mobile')
+        print(e_mobile)
+
+        qs = tbl_employee.objects.filter(id=emp_id).update(
+            name=e_name, 
+            email=e_email, 
+            mobile=e_mobile
+            )
+        
+        if qs:
+            return redirect('retrieve')   # Redirect to retrieve page on success
+
+    else:
+        print(emp_id, '============= update emp_id ==============\n')
+        qs = tbl_employee.objects.get(id=emp_id)
+        return render(request, 'update.html', {'emp_update_data': qs})
